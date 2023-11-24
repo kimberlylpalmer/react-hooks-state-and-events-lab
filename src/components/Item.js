7fa61b968a7f8b6d9f0f3d74b23820a61c132e1f
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-24 15:01:22 -0800

    lab complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+//import {useState}
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  //inCart is the current condition
+  //setInCart will be used to update inCart and rerender the condidtion
+  function handleAddToCart() {
+    setInCart(!inCart);
+  }
+  //the function used to update the cart when the button is clicked
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
+      {/* the className is updated to toggle between "true" and "false" depending on the state */}
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart} className={inCart ? "remove" : "add"}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+        {/* The button is calling the "handleAddToCart" fuction when it's clicked
+        the className is updated according to the state of inCart
+        The button visual is updated according to the state of inCart */}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+//import {useState}
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  //setting up the current state of the selectedCategory - it is default set to "All" to display all categories
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+  // this function is set to the variable handleCategoryChange. it targets the event (e) and updates the setSelectedCategory
+
+  const itemsToDisplay =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
+  //this statement sets the variable "itemsToDisplay" to be set to the current value of selected category. if the category is set to default, it displays all the categories, if it's not, then it's set to display the selected category
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
+          {/* this has been modified to include the eventHandler onChange, and calls the function handleCategoryChange */}
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
+          // this has been updated to use the itemsToDisplay variable instead of all the items
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
